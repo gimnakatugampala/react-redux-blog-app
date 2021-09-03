a53e56a75a76a0f1f6020e7a1e71088371e78a28
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-03 01:24:06 -0700

    fix

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -5,9 +5,7 @@ const ProjectList = () => {
     return (
         <div className="project-list section">
             <ProjectSummary/>
-            
             <ProjectSummary/>
-
             <ProjectSummary/>
         </div>
     )
