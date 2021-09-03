664f209dca24766a1cc38c2003ffb7573df76e04
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-09-02 22:21:59 -0700

    created the file structure & installed the react router DOM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
     return (
+        <BrowserRouter>
         <div>
             <h1>React Redux  With Firebase Project</h1>
         </div>
+        </BrowserRouter>
     )
 }
 
