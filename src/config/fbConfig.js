c6645e302b841908a3c4fef1dccbd1a9919761ba
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-05 17:21:13 -0700

    another small fix

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,7 +1,3 @@
-// import { initializeApp } from 'firebase/app';
-// import {getFirestore } from 'firebase/firestore'
-// import {getAuth } from 'firebase/auth';
-
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
@@ -16,10 +12,6 @@ const firebaseConfig = {
   appId: "1:772581789075:web:6d68798eea48a09d532c66"
 };
 
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-// const auth = getAuth(app)
 
 firebase.initializeApp(firebaseConfig);
 firebase.firestore().settings({ timestampsInSnapshots: true });
