bb9b4b247323f09b5eb1a9a8076df3183f740e23
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-05 19:08:19 -0700

    entered data to the firestore

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,10 +10,15 @@ const projectReducer = (state = initState ,action) => {
     switch (action.type) {
         case 'CREATE_PROJECT':
             console.log('created project',action.project)
-            break;
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('create project error',action.err)
+            return state;
+        default:
+            return state;
     
     }
-    return state
+    // return state
 
 }
 
